47cd9b8a5e37601f0eb28cfa01d5ef7734556bd6
Christopher <olanochristopher@gmail.com>
2023-02-11 22:11:18 +0800

    dividing UI into components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import './style.css';
 
 function App() {
   return (
-    <header className='header'>
-      <div className='logo'>
-        <img src='logo.png' height='68' width='68' alt='Today I Learned Logo' />
-        <h1>Today I Learned</h1>
-      </div>
+    <>
+      {/* HEADER */}
+      <header className='header'>
+        <div className='logo'>
+          <img src='logo.png' height='68' width='68' alt='Today I Learned Logo' />
+          <h1>Today I Learned</h1>
+        </div>
 
-      <button className='btn btn-large btn-open'>Share a fact</button>
-    </header>
+        <button className='btn btn-large btn-open'>Share a fact</button>
+      </header>
+
+      <NewFactForm />
+
+      <main className='main'>
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className='fact-form'>Fact form</form>;
+}
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+function FactList() {
+  return <section>Fact List</section>;
 }
 
 export default App;
