81f60e1cc5206f73d0010597de35c33723c56a48
Christopher Olano <olanochristopher@gmail.com>
2023-04-29 22:08:57 +0800

    redux: convert from use state to redux app component

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+import './style.css';
+import Loader from './component/loader/loader.component';
+import Header from './component/header/header.component';
+import NewFactForm from './component/new-fact-form/new-fact-form.component';
+import CategoryFilter from './component/category-filter/category-filter.component';
+import FactList from './component/fact-list/fact-list.component';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchFactsStart } from './store/facts/facts.reducer';
+import {
+  selectCurrentCategory,
+  selectFactsisLoading,
+  selectShowForm,
+} from './store/facts/facts.selector';
+
+function App() {
+  const dispatch = useDispatch();
+  const currentCategory = useSelector(selectCurrentCategory);
+  const isLoading = useSelector(selectFactsisLoading);
+  const showForm = useSelector(selectShowForm);
+
+  useEffect(() => {
+    dispatch(fetchFactsStart());
+  }, [currentCategory]);
+
+  return (
+    <>
+      <Header />
+
+      {showForm && <NewFactForm />}
+
+      <main className='main'>
+        <CategoryFilter />
+        {isLoading ? <Loader /> : <FactList />}
+      </main>
+    </>
+  );
+}
+
+export default App;
