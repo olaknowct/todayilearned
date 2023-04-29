1b413fa55caffe5dd0e0f36ea6c6b5ae046756d2
Christopher Olano <olanochristopher@gmail.com>
2023-04-29 22:07:19 +0800

    redux: displaying facts

--- src/component/fact-list/fact-list.component.jsx
+++ src/component/fact-list/fact-list.component.jsx
@@ -1,17 +1,22 @@
+import { useSelector } from 'react-redux';
 import Fact from '../fact/fact.component';
+import { selectFactsCount, selectFactList } from '../../store/facts/facts.selector';
 
-const FactList = ({ facts, setFacts }) => {
-  if (facts.length === 0)
+const FactList = () => {
+  const facts = useSelector(selectFactList);
+  const factCounts = useSelector(selectFactsCount);
+
+  if (factCounts === 0)
     return <p className='message'>No facts for this category yet! Create the first one ✌️</p>;
 
   return (
     <section>
       <ul className='facts-list'>
-        {facts.map((fact) => (
-          <Fact key={fact.id} fact={fact} setFacts={setFacts} />
+        {facts?.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
         ))}
       </ul>
-      <p>There are {facts.length} facts in the database. Add your own!</p>
+      <p>There are {factCounts} facts in the database. Add your own!</p>
     </section>
   );
 };
